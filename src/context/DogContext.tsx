99127c7107335fdaa7990d359cf86ac520845102
Kevin Muchene <muchenekevin@gmail.com>
2023-11-26 13:09:33 -0600

    defined specific types of the contexts

--- src/context/DogContext.tsx
+++ src/context/DogContext.tsx
@@ -1,15 +1,51 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState } from "react";
 
-export const DogContext = createContext({
-  favoriteDogs: [],
-  setFavoriteDogs: (newFavoriteDogs) => {},
-});
+interface Dog {
+  img: string;
+  name: string;
+  age: number;
+  zip_code: string;
+  breed: string;
+}
+interface DogContextType {
+  favoriteDogsId: string[];
+  setFavoriteDogsId: (newFavoriteDogs: string[]) => void;
+  aiGeneratedActivities: any;
+  setAiGeneratedActivities: (activities: any) => void;
+  matchDogData: Dog[];
+  setMatchDogData: (dogData: Dog[]) => void;
+}
+const defaultContextValue: DogContextType = {
+  favoriteDogsId: [],
+  setFavoriteDogsId: () => {},
+  aiGeneratedActivities: {},
+  setAiGeneratedActivities: () => {},
+  matchDogData: [],
+  setMatchDogData: () => {},
+};
 
-export const DogProvider = ({ children }) => {
-  const [favoriteDogs, setFavoriteDogs] = useState([]);
+interface DogProviderProps {
+  children: React.ReactNode;
+}
+
+export const DogContext = createContext(defaultContextValue);
+
+export const DogProvider: React.FC<DogProviderProps> = ({ children }) => {
+  const [favoriteDogsId, setFavoriteDogsId] = useState<string[]>([]);
+  const [aiGeneratedActivities, setAiGeneratedActivities] = useState<any>({});
+  const [matchDogData, setMatchDogData] = useState<Dog[]>([]);
 
   return (
-    <DogContext.Provider value={{ favoriteDogs, setFavoriteDogs }}>
+    <DogContext.Provider
+      value={{
+        favoriteDogsId,
+        setFavoriteDogsId,
+        aiGeneratedActivities,
+        setAiGeneratedActivities,
+        matchDogData,
+        setMatchDogData,
+      }}
+    >
       {children}
     </DogContext.Provider>
   );
