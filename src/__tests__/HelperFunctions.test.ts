1f84f131648738153910a07f513c4042e8befa30
Kevin Muchene <muchenekevin@gmail.com>
2023-11-28 12:57:41 -0600

    jest for testing

---
+++ src/__tests__/HelperFunctions.test.ts
@@ -0,0 +1,19 @@
+import { processZipCodes } from "../common/HelperFunctions";
+
+describe("processZipCodes", () => {
+  it("filters out invalid zip codes", () => {
+    const input = "12345 67890 1234 123456 1234a";
+    const expectedOutput = ["12345", "67890"];
+    expect(processZipCodes(input)).toEqual(expectedOutput);
+  });
+
+  it("returns an empty array for no valid zip codes", () => {
+    const input = "1234 abcde 1234a";
+    expect(processZipCodes(input)).toEqual([]);
+  });
+
+  it("handles an empty string input", () => {
+    const input = "";
+    expect(processZipCodes(input)).toEqual([]);
+  });
+});
