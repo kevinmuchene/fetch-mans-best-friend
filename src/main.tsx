fe1e1693e311df2cec768cce977f62b60203dd40
Kevin Muchene <muchenekevin@gmail.com>
2023-12-03 16:23:47 -0600

    Removed react-redux provider to App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Provider } from "react-redux";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { store } from "./redux/Store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
