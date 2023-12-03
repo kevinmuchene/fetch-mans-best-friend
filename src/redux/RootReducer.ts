36c9b696ce012909f071114250c6b4472ed1dc55
Kevin Muchene <muchenekevin@gmail.com>
2023-12-03 11:06:46 -0600

    Setting a matchDogReducer to the rootReducer

--- src/redux/RootReducer.ts
+++ src/redux/RootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import breedsReducer from "./slices/breedDataSlice";
 import favoriteDogsIdReducer from "./slices/favoriteDogsIdSlice";
+import matchDogReducer from "./slices/matchDogSlice";
 export const rootReducer = combineReducers({
   breeds: breedsReducer,
   favoriteDogsId: favoriteDogsIdReducer,
+  matchDog: matchDogReducer,
 });
