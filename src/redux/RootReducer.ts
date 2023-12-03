3435204b796bdf29bc849aa6c40c2a4f52641ffe
Kevin Muchene <muchenekevin@gmail.com>
2023-12-02 20:26:03 -0600

    Added favoriteDogsIdReducer to the rootReducer

--- src/redux/RootReducer.ts
+++ src/redux/RootReducer.ts
@@ -1,5 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import breedsReducer from "./slices/breedDataSlice";
+import favoriteDogsIdReducer from "./slices/favoriteDogsIdSlice";
 export const rootReducer = combineReducers({
   breeds: breedsReducer,
+  favoriteDogsId: favoriteDogsIdReducer,
 });
