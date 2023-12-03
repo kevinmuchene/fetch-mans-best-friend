10396713ab88d0f42069b045be99b29c0d8abae9
Kevin Muchene <muchenekevin@gmail.com>
2023-12-03 12:03:34 -0600

    Removed contexts for matchDogData and filterValues

--- src/context/DogContext.tsx
+++ src/context/DogContext.tsx
@@ -1,34 +1,18 @@
 import { createContext, useState } from "react";
-import { Dog, FilterValues, Location } from "../common/Interfaces";
 
 interface DogContextType {
   aiGeneratedActivities: any;
   setAiGeneratedActivities: (activities: any) => void;
-  matchDogData: Dog[];
-  setMatchDogData: (dogData: Dog[]) => void;
-  matchLocation: Location[];
-  setMatchLocation: (locationData: any) => void;
+
   sortingStrategy: string;
   setSortingStrategy: (strategy: string) => void;
-  filterValues: FilterValues;
-  setFilterValues: (values: any) => void;
+
   initialPageLoadSort: string;
   setInitialPageLoadSort: (strategy: string) => void;
 }
 const defaultContextValue: DogContextType = {
   aiGeneratedActivities: {},
   setAiGeneratedActivities: () => {},
-  matchDogData: [],
-  setMatchDogData: () => {},
-  matchLocation: [],
-  setMatchLocation: () => {},
-  filterValues: {
-    breeds: [],
-    ageMin: "",
-    ageMax: "",
-    validZipCodes: [],
-  },
-  setFilterValues: () => {},
   sortingStrategy: "",
   setSortingStrategy: () => {},
   initialPageLoadSort: "",
@@ -43,15 +27,7 @@ export const DogContext = createContext(defaultContextValue);
 
 export const DogProvider: React.FC<DogProviderProps> = ({ children }) => {
   const [aiGeneratedActivities, setAiGeneratedActivities] = useState<any>({});
-  const [matchDogData, setMatchDogData] = useState<Dog[]>([]);
-  const [matchLocation, setMatchLocation] = useState<any>([]);
   const [sortingStrategy, setSortingStrategy] = useState<string>("asc");
-  const [filterValues, setFilterValues] = useState<FilterValues>({
-    breeds: [],
-    ageMin: "",
-    ageMax: "",
-    validZipCodes: [],
-  });
   const [initialPageLoadSort, setInitialPageLoadSort] = useState<string>("asc");
 
   return (
@@ -59,14 +35,9 @@ export const DogProvider: React.FC<DogProviderProps> = ({ children }) => {
       value={{
         aiGeneratedActivities,
         setAiGeneratedActivities,
-        matchDogData,
-        setMatchDogData,
-        matchLocation,
-        setMatchLocation,
         sortingStrategy,
         setSortingStrategy,
-        filterValues,
-        setFilterValues,
+
         initialPageLoadSort,
         setInitialPageLoadSort,
       }}
