4a244f6c74a5a9de81cfcaf04648e73df8c1cc3c
Kevin Muchene <muchenekevin@gmail.com>
2023-12-03 15:31:07 -0600

    Removed the aiGeneratedActivities state

--- src/context/DogContext.tsx
+++ src/context/DogContext.tsx
@@ -1,9 +1,6 @@
 import { createContext, useState } from "react";
 
 interface DogContextType {
-  aiGeneratedActivities: any;
-  setAiGeneratedActivities: (activities: any) => void;
-
   sortingStrategy: string;
   setSortingStrategy: (strategy: string) => void;
 
@@ -11,8 +8,6 @@ interface DogContextType {
   setInitialPageLoadSort: (strategy: string) => void;
 }
 const defaultContextValue: DogContextType = {
-  aiGeneratedActivities: {},
-  setAiGeneratedActivities: () => {},
   sortingStrategy: "",
   setSortingStrategy: () => {},
   initialPageLoadSort: "",
@@ -26,15 +21,12 @@ interface DogProviderProps {
 export const DogContext = createContext(defaultContextValue);
 
 export const DogProvider: React.FC<DogProviderProps> = ({ children }) => {
-  const [aiGeneratedActivities, setAiGeneratedActivities] = useState<any>({});
   const [sortingStrategy, setSortingStrategy] = useState<string>("asc");
   const [initialPageLoadSort, setInitialPageLoadSort] = useState<string>("asc");
 
   return (
     <DogContext.Provider
       value={{
-        aiGeneratedActivities,
-        setAiGeneratedActivities,
         sortingStrategy,
         setSortingStrategy,
 
