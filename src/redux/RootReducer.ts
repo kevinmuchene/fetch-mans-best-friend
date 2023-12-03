e8eb54bbd282f5dca7c7dbe1dcba6f70562b87b8
Kevin Muchene <muchenekevin@gmail.com>
2023-12-03 12:01:10 -0600

    Added matchLocationReducer and filterValuesReducer to rootReducer

--- src/redux/RootReducer.ts
+++ src/redux/RootReducer.ts
@@ -2,8 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import breedsReducer from "./slices/breedDataSlice";
 import favoriteDogsIdReducer from "./slices/favoriteDogsIdSlice";
 import matchDogReducer from "./slices/matchDogSlice";
+import matchLocationReducer from "./slices/matchLocationSlice";
+import filterValuesReducer from "./slices/filterValuesSlice";
+
 export const rootReducer = combineReducers({
   breeds: breedsReducer,
   favoriteDogsId: favoriteDogsIdReducer,
   matchDog: matchDogReducer,
+  matchLocationData: matchLocationReducer,
+  filterValueData: filterValuesReducer,
 });
