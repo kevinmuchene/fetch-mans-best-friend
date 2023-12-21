e1faddee013bd3bd2ba774719583f746733a7f9c
kevinmuchene <muchenekevin@gmail.com>
2023-12-21 15:46:39 -0600

    refactor: Wrapped <App> with QueryClientProvider and added ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,16 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
