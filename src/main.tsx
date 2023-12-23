ab649478c2480211bfd4bdbc8e7d5ba1496eec62
kevinmuchene <muchenekevin@gmail.com>
2023-12-23 12:02:31 -0600

    feat: Created QueryClient object and passed it to QueryClientProvider. Also, added ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,11 @@ import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 3, retryDelay: 1000, refetchOnWindowFocus: false },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
