06de4db2077208eec407da26bc214a259fd0e7b8
kevinmuchene <muchenekevin@gmail.com>
2023-12-23 12:06:13 -0600

    feat: useFetchDogData to fetch dog data using useQuery

--- src/services/Queries.ts
+++ src/services/Queries.ts
@@ -1,9 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 import DogAction from "../Actions/DogAction";
+import { useAppDispatch, useAppSelector } from "../redux/Hooks";
+import { selectFilterResponseObject } from "../redux/slices/filterResponseObjectSlice";
+import { setNextUrl, setPrevUrl } from "../redux/slices/tableDataPropsSlice";
+
+export interface Dog {
+  id: string;
+  img: string;
+  name: string;
+  age: number;
+  zip_code: string;
+  breed: string;
+}
 
 export function useBreeds() {
   return useQuery({
     queryKey: ["breeds"],
     queryFn: DogAction.fetchBreed,
+  });
+}
+
+export function useFetchDogData() {
+  const { filterResponseObject } = useAppSelector(selectFilterResponseObject);
+  const dispatch = useAppDispatch();
+  dispatch(setNextUrl(filterResponseObject.next));
+  dispatch(
+    setPrevUrl(filterResponseObject.prev ? filterResponseObject.prev : "")
+  );
+
+  return useQuery<Dog[]>({
+    queryKey: ["dogData"],
+    queryFn: () => DogAction.fetchDogs(filterResponseObject.resultIds),
   });
 }
